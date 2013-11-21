889f1644472f314111151eda116c6964bccf6cc9
greelgorke <greelgorke@gmail.com>
2013-11-21 08:38:38 +0100

    version, entrypoint

--- nody.js
+++ nody.js
@@ -1,6 +1,7 @@
-module.exports = { cons   : require('./lib/cons')
-                 , sink   : require('./lib/sink')
-                 , node   : require('./lib/node')
-                 , cond   : require('./lib/cond')
-                 , merge  : require('./lib/merge')
+module.exports = { cons     : require('./lib/cons')
+                 , sink     : require('./lib/sink')
+                 , node     : require('./lib/node')
+                 , cond     : require('./lib/cond')
+                 , merge    : require('./lib/merge')
+                 , subnode  : require('./lib/subnode')
                  }
